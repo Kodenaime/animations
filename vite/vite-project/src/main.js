f5eb247f663871c51c22c44ffee958bee239034f
iamkodenaime <creativedannyk@gmail.com>
2025-01-05 21:33:13 +0100

    started animation with vite vanilla

---
+++ vite/vite-project/src/main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('canvas');
+
+// creating a scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0')
